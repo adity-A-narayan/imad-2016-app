f8e451385eb5c0e93247e55ae366c311fd211e8e
adity-A-narayan <narayan_aditya@ymail.com>
2016-11-27 00:28:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
